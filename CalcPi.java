6b5c5ace308460b166be4d6861f925463b9eeacf
Yoav Wolf <yoavwolf@Yoavs-MacBook-Pro.local>
2025-11-06 17:39:48 +0200

    Completed

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,23 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    
+		//receivng the input and declaring the variables
+		int length = Integer.parseInt(args[0]);
+		double result = 0.0;
+		double fraction;
+		int i = 0;
+
+		//adding as many fractions as defined by the input,
+		//by the order of 1 - 1/3 + 1/5 - 1/7 + ...
+		while(i < length) {
+			fraction = 1.0 / ((i * 2) + 1);
+			if (i % 2 == 0) result += fraction;
+			else result -= fraction;
+			i++;
+		}
+		 //printing the result compared to the real value of PI
+		System.out.println("pi according to Java: " + Math.PI);
+		System.out.println("pi, approximated: " + result * 4);
 	}
 }
--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,31 @@
-//feedback
-
-/*
-Great work boaz!
-*/
-
-
 // Prints a crowd cheering output.
 public class Cheers {
-        public static void main(String[] args) {
-	    // Replace this comment with your code
+    public static void main(String[] args) {
+        
+        //receiving the input and declaring the variables
+        String word = args[0].toUpperCase();
+        int rounds = Integer.parseInt(args[1]);
+        String AnLetters = "AEFHILMNORSX"; //all the letters that need "an" before them 
+        String AorAN = ""; // this variable holds "a " or "an "
+        char letter = ' ';
+        int i = 0;
+
+        //goes over all the letters in the input word and prints by the required format
+        while(i < word.length()) {
+            letter = word.charAt(i);
+            //checks if that letter requires "a" or "an" before it
+            if(AnLetters.indexOf(letter) == -1) AorAN = "a ";
+            else AorAN = "an ";
+            System.out.println("Give me " + AorAN + letter + ": " + letter + "!");
+            i++;
         }
+        System.out.println("What does that spell?");
+
+        //prints the input word X times (X is given via the input)
+        i = 0;
+        while(i < rounds){
+            System.out.println(word + "!!!");
+            i++;
+        }
+    }
 }
